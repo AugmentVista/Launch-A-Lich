f82861e7a5eb9ead3923a9791527a37eb0c99d9c
Silver Burnie <AuAgments@gmail.com>
2025-09-22 14:47:50 -0300

    Distance Marker + Results screen placeholder
    After the player stops moving an arrow will appear and show them how far they reached. One second after that the results screen will appear on the right side.

--- Assets/Scripts/Player/PlayerResultsManager.cs
+++ Assets/Scripts/Player/PlayerResultsManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,8 +8,13 @@ public class PlayerResultsManager : MonoBehaviour
     public GameObject player;
     public Vector2 startPosition;
 
+    Color alpha;
+    float resultsMenuWaitTime = 1f;
+
     public GameObject distanceArrow;
+    public Image resultsMenu;
     public TextMeshProUGUI distanceTraveledThisRunText;
+
 
     Vector2 finalPosition;
 
@@ -19,6 +25,9 @@ public class PlayerResultsManager : MonoBehaviour
         startPosition = new Vector2(player.transform.position.x, player.transform.position.y);
         PlayerStateMachine.OnStopped += ShowDistanceTraveled;
         distanceArrow.SetActive(false);
+        alpha = resultsMenu.color;
+        alpha.a = 0f;
+        resultsMenu.color = alpha;
     }
 
     /// <summary>
@@ -36,13 +45,36 @@ public class PlayerResultsManager : MonoBehaviour
     void ShowDistanceTraveled()
     {
         distanceArrow.SetActive(true);
-
         float distance = RecordedFinalDistanceX();
         distanceTraveledThisRunText.text = $"Distance Traveled: {distance:F1} meters";
+
+        StartCoroutine(ShowResultsMenuAfterDelay(resultsMenuWaitTime));
     }
+
+    IEnumerator ShowResultsMenuAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        ResultsMenu();
+    }
+
+
+    void ResultsMenu()
+    {
+        Color alpha = resultsMenu.color;
+        alpha.a = 1f;
+        resultsMenu.color = alpha;
+    }
+
 
     void Respawn()
     {
         player.transform.position = startPosition;
     }
+
+    private void OnDestroy()
+    {
+        Debug.Log("PlayerResultsManager destroyed — unsubscribing from event.");
+        PlayerStateMachine.OnStopped -= ShowDistanceTraveled;
+    }
+
 }
