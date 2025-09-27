6bc61b52781fff17a7d87dd77347520c6c697586
AshleyMeagan <macnutta@live.com>
2025-09-27 19:44:28 -0300

    Bounce Sound Effect
    Bounce sound effected added

---
+++ Assets/Scripts/Player/PlayerBounce.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerBounce : MonoBehaviour
+{
+    private AudioSource bounceSound;
+
+    void Start()
+    {
+        // Get the AudioSource component from the player
+        bounceSound = GetComponent<AudioSource>();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Play the bounce sound whenever the player collides with something
+        if (bounceSound != null)
+        {
+            bounceSound.Play();
+        }
+    }
+}
